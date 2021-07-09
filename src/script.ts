7c29c00af291a37d2d61d7015080ccfb1fda56d5
Lucas Meyble <lucasmeyble123@gmail.com>
2021-07-09 17:48:11 -0300

    type any e usando types nos parametros de funções

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,24 @@
 let nomes: string[] = ['lucas', 'joao', 'luiz', 'fulano'];
-
 //tem 2 formas de se declarar um array de numbers
 let idade: number[] = [19,20,21,22];
 let numeros: Array<number> = [1,2,3,4];
+
+// -------------------------------------------------------------------------
+
+// ele nao da erro de tipagem nesse push pois eu coloquei a tipagem como nay, ou qualquer coisa
+//usado em ultimo recurso 
+let coisas: any = ['lucas', 'pedro', 'joao'];
+coisas.push(99)
+
+// -------------------------------------------------------------------------
+
+//como colocar o tipo do dado q a função vai receber
+function firstLetterUpperCase(name: string){
+    let firstLetter = name.charAt(0).toUpperCase()
+    return firstLetter+name.substring(1)
+}
+
+firstLetterUpperCase('lucas')
+
+
 
