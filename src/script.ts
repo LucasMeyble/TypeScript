7835ce36f474e0a188ef1535a9d1cf6273c7d5d1
Lucas Meyble <lucasmeyble123@gmail.com>
2021-07-09 17:15:35 -0300

    numbers

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
-let nome: string = 'lucas';
+let nomes: string[] = ['lucas', 'joao', 'luiz', 'fulano'];
 
-let idade: number = 99;
+//tem 2 formas de se declarar um array de numbers
+let idade: number[] = [19,20,21,22];
+let numeros: Array<number> = [1,2,3,4];
 
-let programador: boolean = false;
