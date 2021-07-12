c0f605644920eb573c97f9b0b8579b068d08f17b
Lucas Meyble <lucasmeyble123@gmail.com>
2021-07-12 11:31:11 -0300

    Union Types (múltiplos types)

--- src/script.ts
+++ src/script.ts
@@ -2,14 +2,12 @@ let nomes: string[] = ['lucas', 'joao', 'luiz', 'fulano'];
 //tem 2 formas de se declarar um array de numbers
 let idade: number[] = [19,20,21,22];
 let numeros: Array<number> = [1,2,3,4];
-
 // -------------------------------------------------------------------------
 
 // ele nao da erro de tipagem nesse push pois eu coloquei a tipagem como nay, ou qualquer coisa
 //usado em ultimo recurso 
 let coisas: any = ['lucas', 'pedro', 'joao'];
 coisas.push(99)
-
 // -------------------------------------------------------------------------
 
 //como colocar o tipo do dado q a função vai receber
@@ -19,7 +17,6 @@ function firstLetterUpperCase(name: string){
 }
 
 firstLetterUpperCase('lucas')
-
 // -------------------------------------------------------------------------
 
 //definindo o tipo do retorno de uma função
@@ -27,7 +24,6 @@ function soma (n1: number, n2: number): number {
     return n1 + n2;
 }
 let coisa = soma(90, 10)
-
 // -------------------------------------------------------------------------
 
 const nomes1 = ['lucas', 'pedro', 'joao', 'carlos', 90];
@@ -40,11 +36,10 @@ nomes1.forEach( function(nome){
         console.log(nome)
     }
 })
-
 // -------------------------------------------------------------------------
 
 //tipando os dados q viram no objeto
-//o ponto de interrogação é pra ele perguntar se a propriedade veio ou nao 
+//o ponto de interrogação é pra ele perguntar se a propriedade é opcional  
 function resumo (usuario: {nome: string, idade?: number}){
     if(usuario.idade !== undefined){
         return `Meu nome é ${usuario.nome} e minha idade é ${usuario.idade}.`
@@ -58,3 +53,16 @@ let u = {
 };
 
 resumo(u)
+// -------------------------------------------------------------------------
+
+//o pipe | serve para eu poder dizer q age pode ser tanto string quanto number, logo sou obrigado a fazer uma verificação do tipo da variavel q chegar na func
+function mostrarIdade (age: string | number){
+    if(typeof age === 'string'){
+        console.log(age.toUpperCase())
+    }else{
+        console.log(age)
+    }
+}
+
+mostrarIdade(80);
+mostrarIdade('80')
